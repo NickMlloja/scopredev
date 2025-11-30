ede510000d1e22fac19653057382468b9b45efd2
NickMlloja <71607068+NickMlloja@users.noreply.github.com>
2025-11-30 17:26:37 -0500

    Add eslint rules

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,16 +1,13 @@
 import express from 'express';
-import path from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const app = express();
 const PORT = 8081;
 
-// Get __dirname equivalent in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Serve static files from the frontend folder
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (_req, res) => {
